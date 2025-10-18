abb9103bd267ebf74432dc6b942233dd2b4dd9d4
VeyselKaraali <134809294+VeyselKaraali@users.noreply.github.com>
2025-10-18 21:28:02 +0200

    Include app_video API URLs in project urls.py

--- core/urls.py
+++ core/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app_authentication.api.urls')),
+    path('', include('app_video.api.urls')),
 ]
