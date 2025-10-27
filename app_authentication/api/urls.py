1227b04f85b23c5aaeb4f6afe4c63abbe36c5c7e
VeyselKaraali <134809294+VeyselKaraali@users.noreply.github.com>
2025-10-28 00:37:00 +0100

    Add authentication endpoints (token, password)

--- app_authentication/api/urls.py
+++ app_authentication/api/urls.py
@@ -4,6 +4,9 @@ from .views import (
     ActivateView,
     LoginView,
     LogoutView,
+    TokenRefreshView,
+    PasswordResetView,
+    PasswordConfirmView,
 )
 
 urlpatterns = [
@@ -11,4 +14,7 @@ urlpatterns = [
     path('api/activate/<str:uidb64>/<str:token>/', ActivateView.as_view(), name='activate'),
     path('api/login/', LoginView.as_view(), name='login'),
     path('api/logout/', LogoutView.as_view(), name='logout'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/password_reset/', PasswordResetView.as_view(), name='password_reset'),
+    path('api/password_confirm/<str:uidb64>/<str:token>/', PasswordConfirmView.as_view(), name='password_confirm'),
 ]
