9f937c71a56bbad4528eed6247d4bbd55739d392
VeyselKaraali <134809294+VeyselKaraali@users.noreply.github.com>
2025-11-03 18:13:45 +0100

    Add api/ to endpoints

--- core/urls.py
+++ core/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app_authentication.api.urls')),
-    path('', include('app_video.api.urls')),
+    path('api/', include('app_authentication.api.urls')),
+    path('api/', include('app_video.api.urls')),
 ]
