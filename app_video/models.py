e7a9c7630d053d5479f44ac19b738a5ff1c899bd
VeyselKaraali <134809294+VeyselKaraali@users.noreply.github.com>
2025-11-10 03:15:55 +0100

    Add Video model

--- app_video/models.py
+++ app_video/models.py
@@ -1,3 +1,44 @@
-from django.db import models
+import os
+from datetime import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils.text import slugify
+
+
+def build_upload_path(self, filename, folder):
+    title_slug = slugify(self.title)
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    base, ext = os.path.splitext(filename)
+    new_filename = f"{timestamp}_{slugify(base)}{ext}"
+    return os.path.join(folder, title_slug, new_filename)
+
+def video_upload_path(self, filename):
+    return build_upload_path(self, filename, "videos")
+
+def thumbnail_upload_path(self, filename):
+    return build_upload_path(self, filename, "thumbnails")
+
+
+class Video(models.Model):
+    CATEGORY_CHOICES = [
+        ('action', 'Action'),
+        ('comedy', 'Comedy'),
+        ('documentary', 'Documentary'),
+        ('drama', 'Drama'),
+        ('horror', 'Horror'),
+    ]
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+    video_file = models.FileField(upload_to=video_upload_path, blank=True, null=True)
+    thumbnail = models.ImageField(upload_to=thumbnail_upload_path, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def thumbnail_url(self):
+        if self.thumbnail:
+            return self.thumbnail.url
+        return None
+
+    def __str__(self):
+        return self.title
