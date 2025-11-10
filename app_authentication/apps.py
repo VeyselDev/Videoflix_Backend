fb1a7da25f63d83c7071e362ee9cabcf2457dfcd
VeyselKaraali <134809294+VeyselKaraali@users.noreply.github.com>
2025-11-10 03:21:56 +0100

    Add scheduler for deleting inactive users

--- app_authentication/apps.py
+++ app_authentication/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class AppAuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app_authentication'
+
+    def ready(self):
+        from .scheduler import schedule_delete_inactive_users
+        schedule_delete_inactive_users()
