ace3e9d65d3eb8dda27906fc9d4fb0bda31bc207
VeyselKaraali <134809294+VeyselKaraali@users.noreply.github.com>
2025-11-10 03:23:31 +0100

    Add signals to process and clean up Video files automatically

--- app_video/apps.py
+++ app_video/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AppVideoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app_video'
+
+    def ready(self):
+        from . import signals
---
+++ app_video/signals.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+
+import django_rq
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from app_video.models import Video
+from app_video.tasks import convert_video_to_hls
+
+
+@receiver(post_save, sender=Video)
+def auto_process_video_on_save(sender, instance, created, **kwargs):
+    if created and instance.video_file:
+        queue = django_rq.get_queue('default', autocommit=True)
+        queue.enqueue(convert_video_to_hls, instance.video_file.path)
+
+
+@receiver(post_delete, sender=Video)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    main_dir = os.path.dirname(instance.video_file.path)
+
+    if os.path.exists(main_dir):
+        shutil.rmtree(main_dir, ignore_errors=True)
