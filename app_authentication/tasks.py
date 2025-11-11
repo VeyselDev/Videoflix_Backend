2809eaeb4559f1712841a3a2e122c32b3a79585e
VeyselKaraali <134809294+VeyselKaraali@users.noreply.github.com>
2025-11-11 11:57:11 +0100

    Render HTML activation email with user and activation link

--- app_authentication/tasks.py
+++ app_authentication/tasks.py
@@ -2,20 +2,29 @@ from datetime import timedelta
 
 from django.core.mail import send_mail
 from django.conf import settings
+from django.template.loader import render_to_string
 from django.utils import timezone
 
 from app_authentication.models import CustomUser
 
 
-def send_activation_email(user, html_message):
-    send_mail(
-        subject='Activate your account',
-        message='Please activate your account by clicking the link.',
-        from_email=settings.DEFAULT_FROM_EMAIL,
-        recipient_list=[user.email],
-        html_message=html_message,
-        fail_silently=False,
-    )
+def send_activation_email(user, activation_link):
+    html_message = render_to_string('emails/activation_email.html', {
+        'user': user,
+        'activation_link': activation_link,
+    })
+
+    try:
+        send_mail(
+            subject='Activate your account',
+            message='Please activate your account by clicking the link.',
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[user.email],
+            html_message=html_message,
+            fail_silently=False,
+        )
+    except Exception as e:
+        raise e
 
 def delete_inactive_users():
     cutoff = timezone.now() - timedelta(hours=24)
