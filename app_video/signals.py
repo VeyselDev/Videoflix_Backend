81c0f815b28725dc3affa90aeb5668b28a38a734
VeyselKaraali <134809294+VeyselKaraali@users.noreply.github.com>
2025-11-28 03:41:05 +0100

    Pass instance ID to queue task instead of file path

--- app_video/signals.py
+++ app_video/signals.py
@@ -13,12 +13,12 @@ from app_video.tasks import convert_video_to_hls
 def auto_process_video_on_save(sender, instance, created, **kwargs):
     if created and instance.video_file:
         queue = django_rq.get_queue('default', autocommit=True)
-        queue.enqueue(convert_video_to_hls, instance.video_file.path)
+        queue.enqueue(convert_video_to_hls, instance.id)
 
 
 @receiver(post_delete, sender=Video)
 def auto_delete_file_on_delete(sender, instance, **kwargs):
-    main_dir = os.path.dirname(instance.video_file.path)
+    main_dir = os.path.dirname(instance.id)
 
     if os.path.exists(main_dir):
         shutil.rmtree(main_dir, ignore_errors=True)
