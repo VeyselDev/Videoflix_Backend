89cdaf70cc269538ac06a7178d7565abd649ae88
VeyselKaraali <134809294+VeyselKaraali@users.noreply.github.com>
2025-11-28 03:39:43 +0100

    Add static serving of media files when DEBUG is True

--- core/urls.py
+++ core/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+
+from core import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,3 +26,6 @@ urlpatterns = [
     path('api/', include('app_authentication.api.urls')),
     path('api/', include('app_video.api.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
