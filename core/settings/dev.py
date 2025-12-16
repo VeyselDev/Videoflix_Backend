866a1f162c1b0e372c444d718b07df8c2631367c
VeyselKaraali <134809294+VeyselKaraali@users.noreply.github.com>
2025-12-16 05:00:23 +0100

    Remove STATICFILES_STORAGE

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -7,4 +7,3 @@ CSRF_TRUSTED_ORIGINS = get_list_env('CSRF_TRUSTED_ORIGINS', [])
 CORS_ALLOWED_ORIGINS = get_list_env('CORS_ALLOWED_ORIGINS', [])
 CORS_ALLOW_CREDENTIALS = get_bool_env('CORS_ALLOW_CREDENTIALS', True)
 COOKIE_SECURE = False
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
