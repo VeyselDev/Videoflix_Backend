ac4bc8b0752f4d33a9ac0de025497abbd311d1e3
VeyselKaraali <134809294+VeyselKaraali@users.noreply.github.com>
2026-01-03 21:22:11 +0100

    Update email service

--- app_auth/services/email_service.py
+++ app_auth/services/email_service.py
@@ -1,23 +1,26 @@
 from django.conf import settings
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
+from premailer import transform
 
-from app_auth.services.auth_service import build_password_reset_link
+from app_auth.services.user_service import get_user_from_id_or_fail
 
 
 def send_user_activation_email(user, activation_link):
     """
     Send account activation email to the specified user.
     """
-    template_activation_email = 'emails/account_activation.html'
+    template_activation_email = 'emails/confirm_email.html'
 
-    html_message = render_to_string(template_activation_email, {
+    html_raw = render_to_string(template_activation_email, {
         'user': user,
         'activation_link': activation_link,
     })
 
+    html_message = transform(html_raw)
+
     send_mail(
-        subject='Activate your account',
+        subject='Confirm your email',
         message='Please activate your account by clicking the link.',
         from_email=settings.DEFAULT_FROM_EMAIL,
         recipient_list=[user.email],
@@ -26,17 +29,20 @@ def send_user_activation_email(user, activation_link):
     )
 
 
-def send_password_reset_email(request, user):
+def send_password_reset_email(user_id: int, reset_link):
     """
     Send password reset email.
     """
-    template_password_reset_email = 'emails/password_reset.html'
+    user = get_user_from_id_or_fail(user_id)
+    template_password_reset_email = 'emails/reset_password.html'
 
-    reset_link = build_password_reset_link(request, user)
-    html_message = render_to_string(template_password_reset_email, {
+
+    html_raw = render_to_string(template_password_reset_email, {
         'user': user,
         'reset_link': reset_link,
     })
+
+    html_message = transform(html_raw)
 
     send_mail(
         subject='Password reset',
