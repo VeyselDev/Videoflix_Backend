c143309c4ead8308f62608c471b7d46d8e9745de
VeyselKaraali <134809294+VeyselKaraali@users.noreply.github.com>
2026-01-19 07:54:14 +0100

    Refactor email service

--- app_auth/services/email_service.py
+++ app_auth/services/email_service.py
@@ -10,16 +10,17 @@ from django.template.loader import render_to_string
 from premailer import transform
 
 from app_auth.models import CustomUser
-from app_auth.services.user_service import get_user_or_fail
+from app_auth.services.user_service import get_user_or_404
 from app_auth.utils.email_utils import get_email_local_part
 from app_auth.utils.encoding_utils import encode_int_to_b64
+from core.utils.queue_utils import enqueue_job
 
 
 @dataclass(frozen=True)
 class EmailSchema:
     template: str
     subject: str
-    plain_text: str
+    message: str
     frontend_path: Optional[str] = None
 
 
@@ -32,13 +33,31 @@ EMAIL_SCHEMAS: dict[EmailType, EmailSchema] = {
     EmailType.USER_ACTIVATION: EmailSchema(
         template="app_auth/emails/confirm_email.html",
         subject="Confirm your email",
-        plain_text="Please activate your account by clicking the link.",
+        message=(
+            "Dear {name},\n\n"
+            "Thank you for registering with Videoflix. To complete your registration and verify your email address, "
+            "please click the link below:\n\n"
+            "{url}\n\n"
+            "Please note that this link will expire in 24 hours.\n\n"
+            "If you did not create an account, please ignore this email.\n\n"
+            "Best regards,\n"
+            "Your Videoflix Team"
+        ),
         frontend_path=settings.FRONTEND_USER_ACTIVATION_PATH,
     ),
     EmailType.PASSWORD_RESET: EmailSchema(
         template="app_auth/emails/reset_password.html",
         subject="Password reset",
-        plain_text="Click the link to reset your password.",
+        message=(
+            "Dear {name},\n\n"
+            "We recently received a request to reset your password. If you made this request, "
+            "please click on the following link to reset your password:\n\n"
+            "{url}\n\n"
+            "Please note that this link will expire in 24 hours.\n\n"
+            "If you did not request a password reset, please ignore this email.\n\n"
+            "Best regards,\n"
+            "Your Videoflix Team"
+        ),
         frontend_path=settings.FRONTEND_PASSWORD_RESET_PATH,
     )
 }
@@ -69,19 +88,31 @@ def build_email_context(user: CustomUser, schema: EmailSchema, extra_context: Op
     return context
 
 
-def send_email(email_type_str: str, user_id: int, extra_context: Optional[dict] = None, *, mail_sender=send_mail) -> None:
-    email_type = EmailType(email_type_str)
-    user = get_user_or_fail(pk=user_id)
+def build_email_message(email_type: EmailType, user: CustomUser, extra_context: dict | None = None) -> tuple[str, str, str]:
     schema = EMAIL_SCHEMAS[email_type]
-
     context = build_email_context(user, schema, extra_context)
-    html_message = transform(render_to_string(schema.template, context))
+    message = schema.message.format(**context)
+    html_message = _render_html_body(schema.template, context)
+    return schema.subject, message, html_message
 
-    mail_sender(
-        subject=schema.subject,
-        message=schema.plain_text,
+
+def enqueue_email(*, email_type: EmailType, user_id: int) -> None:
+    enqueue_job(_process_email_job, email_type.value, user_id)
+
+
+def _render_html_body(template_name: str, context: dict) -> str:
+    return transform(render_to_string(template_name, context))
+
+
+def _process_email_job(email_type: EmailType, user_id: int, extra_context: Optional[dict] = None) -> None:
+    user = get_user_or_404(pk=user_id)
+    subject, message, html_message = build_email_message(email_type=email_type, user=user, extra_context=extra_context)
+
+    send_mail(
+        subject=subject,
+        message=message,
         from_email=settings.DEFAULT_FROM_EMAIL,
         recipient_list=[user.email],
         html_message=html_message,
-        fail_silently=False,
+        fail_silently=False
     )
