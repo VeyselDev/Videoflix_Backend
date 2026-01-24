0e9f7023d9dcf7e88c5780ebfa7f49d76ef4415d
VeyselKaraali <134809294+VeyselKaraali@users.noreply.github.com>
2026-01-24 10:17:43 +0100

    Rename RQ_DEFAULT_QUEUE to DEFAULT_QUEUE

--- core/utils/queue_utils.py
+++ core/utils/queue_utils.py
@@ -10,7 +10,7 @@ from rq.job import Job
 def enqueue_job(func: Callable[..., any], *args: any, **kwargs: any) -> Job:
     retry_attempts = 3
     job_timeout_seconds = 120
-    queue = get_queue(settings.RQ_DEFAULT_QUEUE)
+    queue = get_queue(settings.DEFAULT_QUEUE)
 
     return queue.enqueue(
         func,
