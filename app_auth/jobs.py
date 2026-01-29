000241f480d3e6726366e21116b79d98a156ce47
VeyselKaraali <134809294+VeyselKaraali@users.noreply.github.com>
2026-01-29 11:58:09 +0100

    Replace log_info with log_exception

--- app_auth/jobs.py
+++ app_auth/jobs.py
@@ -1,5 +1,5 @@
 from app_auth.services.user_service import delete_inactive_users_older_than
-from core.utils.logging_utils import log_info
+from core.utils.logging_utils import log_info, log_exception
 
 
 def delete_inactive_users_job() -> None:
@@ -7,4 +7,4 @@ def delete_inactive_users_job() -> None:
         deleted_count = delete_inactive_users_older_than(hours=24)
         log_info(f"Cleanup: Deleted {deleted_count} inactive users.")
     except Exception as e:
-        log_info(f"Failed to delete inactive users: {e}", exc_info=True)
+        log_exception(f"Failed to delete inactive users: {e}", exc_info=True)
